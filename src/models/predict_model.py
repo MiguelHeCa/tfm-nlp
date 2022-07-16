7032df5449fe244af008be1050e22140f7b9d808
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2022-07-16 12:45:40 +0200

    Renewing structure

---
+++ src/models/predict_model.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 MiguelHeCa <josemiguel@heca.tech>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Model predictions
+"""
